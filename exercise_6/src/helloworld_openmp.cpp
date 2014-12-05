9aa49934aba25319259e904a54dc50a6fe9dda60
Kayleezee <naumannk@googlemail.com>
2014-12-05 21:22:59 +0100

    Added part 6.1 & part 6.2
    Added OpenMP versions of numerical integration (pi) and hello world

---
+++ exercise_6/src/helloworld_openmp.cpp
@@ -0,0 +1,45 @@
+/*************************************************************************************************
+*
+* Heidelberg University - PCA Exercise 06
+*
+* Group :       PCA03
+* Participant : Klaus Naumann
+*				Jan Brodhaecker
+*
+* File :        helloworld_openmp.cpp
+*
+* Purpose :     HELLO WORLD (Parallel OpenMP Version)
+*
+* Last Change : 05. Dec. 2014
+*
+*************************************************************************************************/
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    int iNumThreads;
+
+    if(argc != 2) {
+        printf("Usage:\n\t");
+        printf("./hellomp\n\t");
+        printf("\t<N number of threads>\n");
+        return EXIT_FAILURE;
+    }
+
+    iNumThreads = atoi(argv[1]);
+
+    omp_set_num_threads(iNumThreads);
+
+    #pragma omp parallel for
+    for(int i = 0; i < iNumThreads; i++) {
+        int iMyId = omp_get_thread_num();
+
+        printf("\nHello World! I'm Thread No. %d", iMyId);
+
+        if(iMyId == 0) {
+            printf("\nTotal amount of active Threads: %d", omp_get_num_threads());
+        }
+    }
+    return EXIT_SUCCESS;
+}
---
+++ exercise_6/src/numerical_integration_openmp.cpp
@@ -0,0 +1,76 @@
+/*************************************************************************************************
+*
+* Heidelberg University - PCA Exercise 06
+*
+* Group :       PCA03
+* Participant : Klaus Naumann
+*				Jan Brodhaecker
+*
+* File :        helloworld_openmp.cpp
+*
+* Purpose :     NUMERICAL INTEGRATION (Parallel OpenMP Version)
+*
+* Last Change : 05. Dec. 2014
+*
+*************************************************************************************************/
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int iNumThreads;
+
+double dx_arctan(int iNumIterations) {
+	double dWidth;
+	double dSum;
+	double dPosition;
+
+	dWidth = 1./iNumIterations;
+	dSum = 0.0;
+	dPosition = 0.0;
+
+    #pragma omp parallel private(dPosition)
+    {
+        #pragma omp parallel for reduction(+:dSum) schedule(static)
+        for(int i = 0; i <= iNumIterations; i++) {
+            int par = omp_in_parallel();
+            printf("\nAm I parallel: %d", par);
+            dPosition += dWidth;
+            dSum += (4. / (1. + dPosition * dPosition)) * dWidth;
+        }
+    }
+
+    int par = omp_in_parallel();
+    printf("\nAm I parallel: %d", par);
+	return dSum;
+}
+
+int main(int argc, char **argv) {
+	int iNumIterations;
+	double dPi;
+	double dStart, dTime;
+
+	if(argc != 3) {
+        printf("Usage:\n\t");
+        printf("./pi\n\t");
+        printf("\t<N number of threads>\n");
+        printf("\t<I number of iterations>\n");
+        return EXIT_FAILURE;
+    }
+
+    iNumThreads = atoi(argv[1]);
+    iNumIterations = atoi(argv[2]);
+
+    omp_set_num_threads(iNumThreads);
+
+    dStart = omp_get_wtime();
+
+    dPi = dx_arctan(iNumIterations);
+
+    dTime = omp_get_wtime() - dStart;
+
+    printf("\nPi (approx.):\t %f", dPi);
+    printf("\nTime needed:\t %f\n", dTime);
+
+	return EXIT_SUCCESS;
+}
